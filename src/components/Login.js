4593d47e9ad3a764ce627a586bd240b8c3ca0957
Jackline-ke <antoninahodhiambo@gmail.com>
2023-05-09 09:39:31 +0300

    Completed assignment

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,28 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-function Login({ setIsLoggedIn }) {
+function Login({ onLogin }) {
   const history = useHistory();
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
 
-  function handleChange(e) {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  }
-
   function handleSubmit(e) {
     e.preventDefault();
 
-    setIsLoggedIn(true);
-
-    // after logging the user in, redirect to the home page!
-    history.push("/");
+    fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((r) => r.json())
+      .then((user) => {
+        onLogin(user);
+        history.push("/home");
+      });
   }
 
   return (
@@ -30,16 +31,8 @@ function Login({ setIsLoggedIn }) {
       <input
         type="text"
         name="username"
-        value={formData.username}
-        onChange={handleChange}
       />
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-      />
-      <button type="submit">Login</button>
+    
     </form>
   );
 }
